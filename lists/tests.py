a66a84f8675534457f1073821ba8148cbc8b022d
Mehdi Sheikh <mehdihs@gmail.com>
2019-06-18 16:49:24 -0400

    Commit before refactoring page 65

--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,12 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
-	def test_home_page_returns_correct_html(self):
-		#request = HttpRequest()
+	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
-		#expected_html = render_to_string('home.html')
-		#self.assertEqual(html, expected_html)
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
+		
